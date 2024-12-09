03c609c8eb460dc8d12aa8ad63c0c0fcdb0598e6
daps <ydapo50@gmail.com>
2024-12-09 10:32:26 +0100

    refactor: improve database connection handling and enhance login limiter middleware comments

--- db/conn.js
+++ db/conn.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import mongoose from "mongoose";
 
 dotenv.config(); // to use the .env file
-
+// Define an asynchronous function named dbConnection
 const dbConnection = async () => {
   try {
+    // Attempt to connect to the MongoDB database using the connection string from environment variables
     await mongoose.connect(process.env.ATLAS_URI);
+    // Log a success message to the console if the connection is successful
     console.log("Database connected successfully.");
+    // Return the mongoose connection object
     return mongoose.connection;
   } catch (error) {
-    "Error while connecting to database", error;
+    // Log an error message to the console if the connection fails
+    console.error("Error while connecting to database", error);
   }
 };
 
--- middlewares/loginLimiter.js
+++ middlewares/loginLimiter.js
@@ -1,9 +1,9 @@
-import { rateLimit } from "express-rate-limit";
+import { rateLimit } from "express-rate-limit"; // Import the rateLimit function from the express-rate-limit package
 
 export const loginLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  limit: 2,
-  message: "Too many login attempts, please try again later.",
-  standardHeaders: "draft-7",
-  legacyHeaders: false,
+  windowMs: 1 * 60 * 1000, // Set the time window for rate limiting to 1 minute (1 * 60 * 1000 milliseconds)
+  limit: 2, // Allow a maximum of 2 requests per windowMs
+  message: "Too many login attempts, please try again later.", // Message to send when the rate limit is exceeded
+  standardHeaders: "draft-7", // Use the standard rate limit headers (draft-7)
+  legacyHeaders: false, // Disable the legacy rate limit headers
 });
